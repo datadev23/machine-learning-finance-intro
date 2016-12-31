3ced637f83f46cffc2bdf83aedea35ca4e21be5d
devenv <devenv@devenv-VirtualBox.(none)>
2016-12-31 22:37:36 +0000

    numpy array

---
+++ numparray.py
@@ -0,0 +1,10 @@
+# This tutorial shows how to develop array using the numerical package
+import numpy as np
+
+def test_run():
+	print np.array([(1,2,3),(4,5,6)])
+        
+
+if __name__ == "__main__":
+	test_run()
+	
